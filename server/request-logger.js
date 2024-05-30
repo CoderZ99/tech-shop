c63abf2bf543c1a012f46bb6c69c94d68da7a7ea
CoderZ99 <phuc99ute@gmail.com>
2024-05-31 00:52:31 +0700

    fix: edit log dir

--- server/request-logger.js
+++ server/request-logger.js
@@ -8,7 +8,7 @@ const fs = require('fs')
  */
 function setupRequestLogger() {
     // Create a write stream (in append mode) to a log file in the log folder
-    const logFolderPath = path.join(__dirname, '../log');
+    const logFolderPath = path.join(__dirname, '/logs');
     const logFilePath = path.join(logFolderPath, 'request.log');
     fs.mkdirSync(logFolderPath, { recursive: true }); // Create the log folder if it doesn't exist
     const accessLogStream = fs.createWriteStream(logFilePath, { flags: 'a' });
